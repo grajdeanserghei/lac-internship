49de4d2a9cb9f4d6d0d21010a6022ceb4619a029
Serghei Grajdean <sergiu.grajdean@amdaris.com>
2018-04-26 09:12:17 +0300

    controllers prepared

---
+++ src/MlApiComp/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MlApiComp.Controllers
+{
+    [Route("api/demo")]
+    public class DemoController: Controller
+    {
+        // Access me by typing http://yourhost:port/api/demo/helloworld?message=yourmessage E.g. http://localhost:26497/api/demo/helloworld?message=Bob
+
+        [Route("helloworld")]
+        public IActionResult HelloWorld(string message)
+        {
+            return Ok("Hello world! " + message);
+        }
+    }
+}
---
+++ src/MlApiComp/Models/DemoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MlApiComp.Models
+{
+    public class DemoModel
+    {
+        public int Id { get; set; }
+    }
+}
