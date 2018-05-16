f9c9ff6a91a8b80e1ae49eff210eaaafffac8602
Serghei Grajdean <grajdeanserghei@gmail.com>
2018-05-16 08:19:35 +0300

    swagger added to api, bug fixed in ml-demo component

--- src/MlApiComp/ClientApp/src/app/ml-demo/ml-demo.component.ts
+++ src/MlApiComp/ClientApp/src/app/ml-demo/ml-demo.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { read } from 'fs';
 import { MlService } from '../services/ml.service';
 
 
--- src/MlApiComp/Startup.cs
+++ src/MlApiComp/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MlApiComp
 {
@@ -25,6 +26,12 @@ namespace MlApiComp
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +48,16 @@ namespace MlApiComp
 
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc(routes =>
             {
